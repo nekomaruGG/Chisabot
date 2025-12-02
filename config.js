bf257ec34bd75d825b63427d473ab61e7e4a09cd
Ariuysyuuuu <xyraakanghealers@gmail.com>
2025-12-03 01:21:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["1056938161672044544", ""], 
+  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
@@ -12,7 +12,7 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://dc.gg/posneko",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
