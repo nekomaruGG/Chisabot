88a5ef011db2928d7d8af690bc406abb0e34916b
Ariuysyuuuu <xyraakanghealers@gmail.com>
2025-12-03 01:54:26 +0800

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const supportServerLink = "https://dc.gg/posneko";
+            const githubLink = "https://github.com/nekomaru";
+            const replitLink = "https://replit.com/@nekomaru";
+            const youtubeLink = "https://www.youtube.com/@nekomaru";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
