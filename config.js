88da8a1baf526339c9ea285277143589a6083eb4
Ariuysyuuuu <xyraakanghealers@gmail.com>
2025-12-03 09:04:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Developer: @neko.lua", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://dc.gg/posneko",
   embedTimeout: 5, 
